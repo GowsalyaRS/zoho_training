a22130597f691c74446f206238f4caf6271b0bff
GowsalyaRS <gowsalya7701@gmail.com>
2024-09-18 14:56:16 +0530

    modify loose coupling

--- com/oops/coupling/LoseCouplingDemo.java
+++
@@ -1,9 +0,0 @@
-package com.oops.coupling;
-public class LoseCouplingDemo {
-    public static void main(String[] args) 
-    {
-       Doucumentable doucument = new WordDoucument();
-       PrintDoucument printDoucument = new PrintDoucument(doucument);
-       printDoucument.print();
-    }
-}
